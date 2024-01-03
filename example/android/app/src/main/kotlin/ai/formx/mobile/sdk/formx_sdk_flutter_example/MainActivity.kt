7b8b024acadac249d616cabd2736a85f47e501aa
mark wu <saybyebyet@gmail.com>
2024-01-03 11:43:02 +0800

    Fix cannot open photo gallery from native camera view

--- example/android/app/src/main/kotlin/ai/formx/mobile/sdk/formx_sdk_flutter_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/ai/formx/mobile/sdk/formx_sdk_flutter_example/MainActivity.kt
@@ -1,6 +1,5 @@
 package ai.formx.mobile.sdk.formx_sdk_flutter_example
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterFragmentActivity()
